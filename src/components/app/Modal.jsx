4c14e309f82a69187401b5cfd5f25e117968e36a
Alireza Tabrizi <alias1371@gmail.com>
2022-08-31 20:24:18 +0430

    add function for adding new question

--- src/components/app/Modal.jsx
+++ src/components/app/Modal.jsx
@@ -3,12 +3,56 @@ import BoxHeader from "./BoxHeader.jsx";
 import Button from "./Button.jsx";
 import Close from "../svgs/Close.jsx";
 import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {fetchCreatePost} from "../../stores/feed/posts.js";
 
 function Modal() {
     const dispatch = useDispatch()
-    const {data , show} = useSelector(state => {
+    const {data, show} = useSelector(state => {
         return state.modal
     })
+    const {currentUser} = useSelector(state => state.users)
+    const [subject, setSubject] = useState('')
+    const [body, setBody] = useState('')
+    let SDebounce = false;
+    let BDebounce = false;
+
+    const creatQuestion = () => {
+        const data = {
+            title: subject,
+            author: currentUser.name,
+            author_id: currentUser.id,
+            body,
+            created: Date.now(),
+            image_url: "/3.png",
+            comments: 0
+        }
+        dispatch(fetchCreatePost(data))
+        closeModal()
+    }
+    const handleSubject = (e) => {
+        if (SDebounce) {
+            return
+        }
+        SDebounce = true;
+        setSubject(e.target.value)
+
+        setTimeout(() => {
+            SDebounce = false;
+        }, 1000)
+    }
+
+    const handleBody = (e) => {
+        if (BDebounce) {
+            return
+        }
+        BDebounce = true;
+        setBody(e.target.value)
+
+        setTimeout(() => {
+            BDebounce = false;
+        }, 1000)
+    }
 
     const closeModal = (e) => {
         dispatch({type: 'hide'})
@@ -25,23 +69,26 @@ function Modal() {
                             <BoxHeader>
                                 <h3 className="text-base font-bold">ایجاد سوال جدید</h3>
                                 <div onClick={closeModal} className="cursor-pointer">
-                                    <Close />
+                                    <Close/>
                                 </div>
                             </BoxHeader>
                             <div className="pt-4 pb-6 px-5">
                                 <label className="mb-3.5 block">
                                     <span className="block text-[#454545] text-xs mb-2.5">موضوع</span>
-                                    <input type="text" placeholder="مشکل در اجرای کد" className="bg-white w-full border border-[#EEEEEE] rounded-md px-4 py-3 text-sm"/>
+                                    <input type="text" onInput={handleSubject} placeholder="مشکل در اجرای کد"
+                                           className="bg-white w-full border border-[#EEEEEE] rounded-md px-4 py-3 text-sm"/>
                                 </label>
                                 <label className="mb-3.5 block">
                                     <span className="block text-[#454545] text-xs mb-2.5">موضوع</span>
-                                    <textarea type="text" placeholder="مشکل در اجرای کد" className="bg-white w-full border border-[#EEEEEE] rounded-md px-4 py-3 text-sm h-[164px]"/>
+                                    <textarea onInput={handleBody} type="text" placeholder="مشکل در اجرای کد"
+                                              className="bg-white w-full border border-[#EEEEEE] rounded-md px-4 py-3 text-sm h-[164px]"/>
                                 </label>
                                 <div className="flex justify-end items-center">
-                                    <div onClick={closeModal} className="ml-12 cursor-pointer text-xs text-[#27AE60] font-bold">
+                                    <div onClick={closeModal}
+                                         className="ml-12 cursor-pointer text-xs text-[#27AE60] font-bold">
                                         انصراف
                                     </div>
-                                    <Button>ایجاد سوال</Button>
+                                    <Button onClick={creatQuestion}>ایجاد سوال</Button>
                                 </div>
                             </div>
                         </Box>
--- src/components/feed/Item.jsx
+++ src/components/feed/Item.jsx
@@ -4,6 +4,7 @@ import Box from "../app/Box.jsx";
 import Button from "../app/Button.jsx";
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
+import moment from "moment-jalaali";
 
 
 function Item(props) {
@@ -12,15 +13,15 @@ function Item(props) {
         <Box>
             <BoxHeader>
                 <div className="flex items-center">
-                    <img src={users.find(user => user.id === props.post.author_id).image_url} alt="" className="w-[32px] h-[32px] object-cover rounded-lg"/>
+                    <img src={users.find(user => user.id === props.post.author_id)?.image_url} alt="" className="w-[32px] h-[32px] object-cover rounded-lg"/>
                     <h3 className="text-base font-bold mr-4">{props.post.title}</h3>
                 </div>
                 <div className="flex text-xs text-[#777777]">
                     <div className="after:w-px after:h-full after:mx-3 after:bg-black/10 after:block flex">
-                        ساعت : <span className="text-[#222222] font-bold mr-1">۱۶:۴۸</span>
+                        ساعت : <span className="text-[#222222] font-bold mr-1">{ props.post.created ? moment( props.post.created).format('hh:mm') : ''}</span>
                     </div>
                     <div>
-                        تاریخ : <span className="text-[#222222] font-bold">۱۴۰۰/۲/۱۵</span>
+                        تاریخ : <span className="text-[#222222] font-bold">{ props.post.created ? moment( props.post.created).format('jYYYY/jM/jD') : ''}</span>
                     </div>
                     <div className="flex items-center mr-7">
                         <Comment />
